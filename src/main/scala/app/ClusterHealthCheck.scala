1fe3cd9171e0cf3ec5d5f4ba984df18af10b6d1b
Julian Sarrelli <julian.sarrelli@tiendanube.com>
2021-06-05 15:54:27 -0300

    split brain resolver

---
+++ src/main/scala/app/ClusterHealthCheck.scala
@@ -0,0 +1,18 @@
+package app
+
+import akka.actor.ActorSystem
+import akka.cluster.{Cluster, MemberStatus}
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.Future
+
+// Enabled in application.conf
+class ClusterHealthCheck(system: ActorSystem) extends (() => Future[Boolean]) {
+  private val log = LoggerFactory.getLogger(getClass)
+  private val cluster = Cluster(system)
+
+  override def apply(): Future[Boolean] = {
+    log.info("ClusterHealthCheck called")
+    Future.successful(cluster.selfMember.status == MemberStatus.Up)
+  }
+}
