cd93f21013d972272f8335baf4e442742e7f228e
Julian Sarrelli <julian.sarrelli@tiendanube.com>
2021-07-21 10:54:40 -0300

    formig cluster

--- scripts/start_cluster.sh
+++ scripts/start_cluster.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 set -exu
-eval $(minikube -p minikube docker-env)
+#eval $(minikube -p minikube docker-env)
 sbt docker:publishLocal
 
-export KUBECONFIG=~/.kube/config
+#export KUBECONFIG=~/.kube/config
 kubectl config set-context docker-desktop
 
 kubectl apply -f kubernetes/namespace.json
-kubectl config set-context --current --namespace=todolist-app-1
+kubectl config set-context --current --namespace=todolist-app
 kubectl apply -f kubernetes/akka-cluster.yml
 kubectl expose deployment todolist-app --type=LoadBalancer --name=todolist-app-service
 
 
 #minikube service todolist-app-service -n todolist-app-1 --url
+#minikube start --network bridge
